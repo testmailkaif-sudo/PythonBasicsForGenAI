17e4ec045824975180c61af10b986aa403d1b052
Mohammad Kaif Shaik Podile Peddavali <mohammadkaif.sp@plintron.com>
2026-03-01 19:32:29 +0530

    First commit

---
+++ Basics/variables.py
@@ -0,0 +1,15 @@
+"Variables are the like a container to store the specific type of data"
+
+num = 10
+print(num)
+pi = 3.14
+print(pi)
+name = "kaif"
+print(name)
+is_active = True
+print(is_active)
+is_deactivate = False
+print(is_deactivate)
+complex_numbers = 5+3j
+print(complex_numbers.real)
+print(complex_numbers.imag)
