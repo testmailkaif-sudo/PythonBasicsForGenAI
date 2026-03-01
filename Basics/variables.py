5e378df478e4468a31800cdf3ab2932f3e67ef0f
Mohammad Kaif Shaik Podile Peddavali <mohammadkaif.sp@plintron.com>
2026-03-01 19:36:57 +0530

    Revert "First commit"
    This reverts commit 07a5df9e16b606e04396a55096e41ed5b8504c04.

--- Basics/variables.py
+++ Basics/variables.py
@@ -1,15 +1,15 @@
 "Variables are the like a container to store the specific type of data"
 
 num = 10
-print(num); "num is a variable that stores the integer type of data"
+print(num)
 pi = 3.14
-print(pi);"pi is a variable that stores the float type of data"
+print(pi)
 name = "kaif"
-print(name);"name is a variable that stores the string type of data"
+print(name)
 is_active = True
-print(is_active); "is_active and is_deactivate both the variables store Boolean type of data"
+print(is_active)
 is_deactivate = False
 print(is_deactivate)
-complex_numbers = 5+3j; "complex_numbers is the variable that stores the complex data"
+complex_numbers = 5+3j
 print(complex_numbers.real)
 print(complex_numbers.imag)
